e33d2be1cc8ca95a5e7cea17e8128b357a5ee708
Agostino <agostinoscholes572@gmail.com>
2025-09-24 09:26:06 +0300

    Added Watchlist page

---
+++ frontend/src/components/Watchlist.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Watchlist() {
+  return (
+    <div>
+      <h2>Your Watchlist </h2>
+      <p>Movies you want to watch will appear here.</p>
+    </div>
+  );
+}
+
+export default Watchlist;
