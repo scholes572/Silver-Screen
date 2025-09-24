4d7a6e45bcf4ea05d8e2f745ae3c09ec234a1db6
Agostino <agostinoscholes572@gmail.com>
2025-09-24 09:22:39 +0300

    Added Navbar with links to Home, Movies, and Watchlist

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav style={styles.nav}>
+      <h1 style={styles.logo}>SilverScreen</h1>
+      <div>
+        <Link to="/" style={styles.link}>Home</Link>
+        <Link to="/movies" style={styles.link}>Movies</Link>
+        <Link to="/watchlist" style={styles.link}>Watchlist</Link>
+      </div>
+    </nav>
+  );
+}
+
+const styles = {
+  nav: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "10px 20px",
+    backgroundColor: "#222",
+    color: "white",
+  },
+  logo: {
+    margin: 0,
+  },
+  link: {
+    margin: "0 10px",
+    textDecoration: "none",
+    color: "white",
+    fontWeight: "bold",
+  }
+};
+
+export default Navbar;
